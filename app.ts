03958f75a9134c855f9bd83064d69f0b806d1d75
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-02-21 16:25:20 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,5 @@
+//exercise 10
+//my name is ALISHA
+//Dated: 18/02/2024
+// this program will run simple code just like hello world
+console.log(`hello world simple program`);
---
+++ program2.js
@@ -0,0 +1,5 @@
+//exercise 10
+//my name is ALISHA
+//Date: 18/02/2024
+//this program will multiplication
+console.log(5 * 2);
